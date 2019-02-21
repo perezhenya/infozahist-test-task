6f3213f69d015d626bc6c23d9dcd51dc0226fd40
Kostyantyn Semergiy <ksemergiy@warfare-tec.com>
2019-02-21 15:57:27 +0200

    FIX cors put

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
 
-var indexRouter = require('./routes/index');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
-})
-app.use('/', indexRouter);
+});
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 
 module.exports = app;
